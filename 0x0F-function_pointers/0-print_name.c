afb7d431eac76274c4978431d1a872fc6d6ba62c
Ignatius Okon <ignaokon@yahoo.com>
2023-01-18 16:43:28 -0800

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {

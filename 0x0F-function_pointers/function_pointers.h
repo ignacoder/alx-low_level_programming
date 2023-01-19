d6b60f42cb9ecf45d2c06fe2a29e880ab39ebf2e
Ignatius Okon <ignaokon@yahoo.com>
2023-01-18 16:06:09 -0800

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,6 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 
-#include <stdlib.h>
-#include <stddef.h>
-
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));

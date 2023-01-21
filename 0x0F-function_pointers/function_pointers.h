f2e798b796f57f67b1215c8cfc67e2b5e2a35007
Ignatius Okon <ignaokon@yahoo.com>
2023-01-21 13:37:33 -0800

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,4 +10,15 @@ int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+ */
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
 #endif 
